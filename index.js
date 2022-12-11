5e00bdd9af134de64cd14d92fb5cc0ebe845f7ff
dikshant4901 <dikshant1865.be20@chitkara.edu.in>
2022-12-11 16:11:25 +0530

    Views Folder

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+// const path =require('path');
 //use express router
 app.use('/',require('./routes'));
+
+
+//Set-up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
