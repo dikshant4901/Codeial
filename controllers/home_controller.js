2fb65c8086dbea1d2339c8890c9071c3208ac466
Aadish19 <adish8511@gmail.com>
2022-12-14 02:04:13 +0530

    made a navbar so assets for home.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,5 @@
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title:'Home'
-    });
+    return res.render('home');
 }
 
 //module.exports.actionName = function(req,res){}
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+
 // const path =require('path');
 //use express router
-app.use('/',require('./routes'));
+
 
 
 //Set-up the view engine
@@ -12,9 +13,10 @@ app.set('views','./views');
 
 // app.get('/',(req,res));
 
+// app.use(express.urlencoded()); // middleware
+app.use(express.static('assets')); //css and js files 
 
-
-
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
